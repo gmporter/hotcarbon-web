ac00dbce59138ac46053e840b49d612573ec1c86
George Porter <gmporter@cs.ucsd.edu>
2023-04-18 11:32:40 -0400

    Updated the upload scripts

--- 2022/upload-webpage.sh
+++ 2022/upload-webpage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -ravz --delete -e "ssh -p 222" --exclude=upload-webpage.sh public/ trolley.sysnet.ucsd.edu:/na/proj/hotcarbon/website
+rsync -ravz --delete -e "ssh -p 222" --exclude=upload-webpage.sh public/ trolley.sysnet.ucsd.edu:/na/proj/hotcarbon/website/2022
